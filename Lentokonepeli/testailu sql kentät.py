d081e49583e527e7500486e52736379b1e3a0dfa
maxlindblad <max.lindblad@metropolia.fi>
2023-02-13 20:42:57 +0200

    j

--- Lentokonepeli/testailu sql kentät.py
+++ Lentokonepeli/testailu sql kentät.py
@@ -1,10 +1,15 @@
 import mysql.connector
 
 
-
+nykyinen_lon = "24.957996168"
+nykyinen_len = "60.316998732"
+airport_type = "heliport"
+etäisyys = "300"
 def airports():
-    sql = "select name, ST_Distance_Sphere( point ('24.957996168', '60.316998732'), point(longitude_deg, latitude_deg)) * .001"
-    sql += "as `distance_in_km` from `airport` where type = 'heliport' having `distance_in_km` <= '300'"
+    sql = "select name, ST_Distance_Sphere( point ('" + nykyinen_lon +"','" + nykyinen_len + "'),"
+    sql += "point(longitude_deg, latitude_deg)) * .001"
+    sql += "as `distance_in_km` from `airport` "
+    sql += "where type = '" + airport_type + "' having `distance_in_km` <= '" + etäisyys + "'"
     sql += "order by `distance_in_km` asc"
 
     print(sql)
