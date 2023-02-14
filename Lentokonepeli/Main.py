0fb010a770b91188f03d1fc83f5b8514c2728961
maxlindblad <max.lindblad@metropolia.fi>
2023-02-14 12:46:57 +0200

    j

--- Lentokonepeli/Main.py
+++ Lentokonepeli/Main.py
@@ -1,1 +1,16 @@
-print("hello")
+
+
+class User:
+    def __init__(self, name, money, time, player_location, upgrades, risk, co_2):
+        self.name = name
+        self.money = money
+        self.time = time
+        self.player_location = player_location
+        self.upgrades = upgrades
+        self.risk = risk
+        self.co_2 = co_2
+
+    def Upgrade(self,):
+
+
+        return
