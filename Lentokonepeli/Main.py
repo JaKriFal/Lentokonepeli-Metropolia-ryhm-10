180d391d9c2eac42caa1aa562ebdd420b3c623f2
jankf <Jan.Falck@metropolia.fi>
2023-02-16 14:06:34 +0200

    valitsin-funktio rakennettu

--- Lentokonepeli/Main.py
+++ Lentokonepeli/Main.py
@@ -4,6 +4,22 @@
 # Tänne globaalien muuttujien alustaminen
 vic_con = False
 
+# Tänne funktiot
+
+def valitsin(User): #nää on ihan placeholdereita vielä, tehdään kaikille toiminnoille omat funktiot Userille
+    print("Valitse komento: \n Lennä \n Tiedot \n Apua \n Lopeta")
+    valinta = input("Anna komento: ")
+    if valinta == "Lennä":
+        User.player_location = input("Anna kohde")
+    elif valinta == "Tiedot":
+        User.tulosta_tiedot()
+    elif valinta == "Apua":
+        print("APUVA!!!")
+    elif valinta == "Lopeta":
+        User.lopeta_peli()
+    else:
+        print("Komentoa ei tunnistettu")
+
 # Tänne luokka-alustukset (varmaan lähinnä User/Pelaaja)
 
 class User:
@@ -16,13 +32,14 @@ class User:
         self.risk = 0
         self.co_2 = 0
         self.co_2_rate = 0
+        self.vic_con = False
 
     def lopeta_peli(self):
         quitornot = input("Lopetetaanko peli? Y/N: ")
         if quitornot == "Y":
-            return True
+            self.vic_con = True
         elif quitornot == "N":
-            return False
+            self.vic_con = False
         else:
             print("Komentoa ei tunnistettu")
 
@@ -36,13 +53,8 @@ name = input("Anna pelaajan nimi:")
 Pelaaja = User("name")
 
 # Main loop
-while vic_con == False:
-    testi = Pelaaja.lopeta_peli()
-    if testi:
-        vic_con = True
-        print("Peli lopetettu")
-    else:
-        print("Peli jatkuu")
+while Pelaaja.vic_con == False:
+    valitsin(Pelaaja)
 
 
 #Tänne toiminnot jotka ajetaan kun pelikerta päättyy
