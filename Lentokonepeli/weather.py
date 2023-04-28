2c9492eb9767668730d28b98e34134938800f991
Maxlindbl <max.lindblad@metropolia.fi>
2023-04-28 19:32:08 +0300

    testi

--- Lentokonepeli/weather.py
+++ Lentokonepeli/weather.py
@@ -1,14 +1,17 @@
-import requests
-from Main import User
-# Path: Lentokonepeli/Main.py
-def weather(User.player_location):
-    city = User.player_location
-#location = geocode(city)
-#print(location.adress)
 
-    weather = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&lang=fi&APPID=7c449d15551f18e130e9229fa2887cc3"
+from pip._vendor import requests
+# Path: Lentokonepeli/Main.py
+
+
+def Get_weather(player_location):
+    city = player_location
+# location = geocode(city)
+# print(location.adress)
+
+    weather = "https://api.openweathermap.org/data/2.5/weather?q=" + \
+        city + "&lang=fi&APPID=7c449d15551f18e130e9229fa2887cc3"
     request = requests.get(weather).json()
-#print(request)
+# print(request)
 
     temperature = request["main"]
     celsius = temperature["temp"]
@@ -18,5 +21,3 @@ def weather(User.player_location):
 
     print("Sää " + city + " on " + str(final_temp) + " astetta ja " + desc2 + ".")
     return final_temp, desc2
-
-
