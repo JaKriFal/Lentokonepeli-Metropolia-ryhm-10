c49fbdd79d510d8066561712f71e31e60ad88111
Maxlindbl <max.lindblad@metropolia.fi>
2023-05-04 20:08:24 +0300

    ok

--- Lentokonepeli/gamesite.js
+++ Lentokonepeli/gamesite.js
@@ -5,31 +5,24 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 map.setView([60, 24], 7);
 
-// lisää markeri helsinki vantaa lentokenttä
-const marker = L.marker([60.317222, 24.963333]).addTo(map);
-marker.bindPopup('Helsinki-Vantaan lentokenttä').openPopup();
+// JSON-pyynnön tekeminen endpointiin
+$.getJSON("http://127.0.0.1:3000/kokeilu/", function(data) {
 
-// lisää markeri tampereen lentokenttä
-const marker2 = L.marker([61.414167, 23.604167]).addTo(map);
-marker2.bindPopup('Tampere-Pirkkalan lentokenttä').openPopup();
+    // Käsitellään vastausdatan pisteet
+    var points = data.lista_kentistä.map(function(point) {
+        return {
+            name: point[0],
+            latlng: L.latLng(point[2], point[3]),
+            risk: data.riskilista_kentille[data.lista_kentistä.indexOf(point)],
+            distance: point[1]
+        };
+    });
 
-const marker3 = L.marker([60.317222, 24.963333], { color: 'blue' }).addTo(map);
-marker3.bindPopup('Helsinki-Vantaan lentokenttä').openPopup();
-
-const marker4 = L.marker([61.414167, 23.604167], { color: 'blue' }).addTo(map);
-marker4.bindPopup('Tampere-Pirkkalan lentokenttä').openPopup();
-
-// lisää polyline helsinki vantaa lentokenttä ja tampereen lentokenttä
-const polyline = L.polyline([
-  [60.317222, 24.963333],
-  [61.414167, 23.604167],
-]).addTo(map);
-
-
-const polyline2 = L.polyline([
-  [60.317222, 24.963333],
-  [61.414167, 23.604167],
-]).addTo(map);
-
-
-
+    // Pisteiden piirtäminen kartalle
+    for (var i = 0; i < points.length; i++) {
+        var point = points[i];
+        L.marker(point.latlng)
+            .addTo(map)
+            .bindPopup(point.name + "<br>Riski jäädä kiinni: " + point.risk + "<br>Etäisyys: " + point.distance.toFixed(2) + " km");
+    }
+});
